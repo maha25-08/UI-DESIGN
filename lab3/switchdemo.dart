19b497d4eacb2061d038f08236165e86ebff60c9
maha25-08 <mahalakshmikothapeta@gmail.com>
2025-03-06 11:57:59 +0530

    Add files via upload

---
+++ lab3/switchdemo.dart
@@ -0,0 +1,15 @@
+class switchdemo{
+   	void  sdemo(String name){
+	switch(name){
+      		case 'apple':
+                  	print("apples are available in shop");
+     		case 'mango':
+                  	print("mangoes are available in shop");
+     		case 'berries':
+                  	print("berries are available in shop");	
+     		  	break;
+     		default:
+                  	print("The fruit is not listed in the shop");
+    		}
+	}
+}
